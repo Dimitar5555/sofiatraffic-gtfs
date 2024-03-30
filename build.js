7e2d6cec597eb4315ff3c75273d92e01995b8788
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-03-30 18:59:14 +0200

    Add files via upload

--- build.js
+++ build.js
@@ -8,6 +8,14 @@ const GTFSTypes = {
 	'trolleybus': 11,
 	'autobus': 3
 };
+const GTFSConsts = {
+	agency: {
+		name: 'Център за градска мобилност',
+		site: 'https://sofiatraffic.bg'
+	},
+	timezone: 'Europe/Sofia',
+	lang: 'bg'
+}
 var promises = [];
 
 function getJSON(file){
@@ -64,45 +72,45 @@ promises.push(
 
 
 var trips;
-getJSON('trips.json')
-.then(data => {
-	trips = data;
-});
-
 var directions;
-getJSON('directions.json')
-.then(data => {
-	directions = data;
-});
-getJSON('stop_times.json')
-.then(data => {
-	let last_trip = 0;
-	let stop_times_data = [];
-	let trips_data = [];
-	trips_data.push(['route_id', 'service_id', 'trip_id']);
-	stop_times_data.push(['trip_id', 'arrival_time', 'departure_time', 'stop_id', 'stop_sequence']);
-	data.forEach(stop_times => {
-		last_trip++;
-		trips_data.push([trips[stop_times.trip].route_index+1, 1, last_trip]);
-		let stops = directions.find(dir => dir.code==trips[stop_times.trip].direction).stops;
-		stop_times.times.forEach((stop_time, index) => {
-			stop_times_data.push([last_trip, minsToTime(stop_time), minsToTime(stop_time), stops[index], index+1]);
+promises.push(
+	getJSON('trips.json')
+	.then(data => {
+		trips = data;
+		return getJSON('directions.json');
+	})
+	.then(data => {
+		directions = data;
+		return getJSON('stop_times.json');
+	})
+	.then(data => {
+		let last_trip = 0;
+		let stop_times_data = [];
+		trips_data.push(['route_id', 'service_id', 'trip_id']);
+		let trips_data = [];
+		stop_times_data.push(['trip_id', 'arrival_time', 'departure_time', 'stop_id', 'stop_sequence']);
+		data.forEach(stop_times => {
+			last_trip++;
+			trips_data.push([trips[stop_times.trip].route_index+1, 1, last_trip]);
+			let stops = directions.find(dir => dir.code==trips[stop_times.trip].direction).stops;
+			stop_times.times.forEach((stop_time, index) => {
+				stop_times_data.push([last_trip, minsToTime(stop_time), minsToTime(stop_time), stops[index], index+1]);
+			});
 		});
-	});
-	promises.push(saveToFile('stop_times', stop_times_data));
-	promises.push(saveToFile('trips', trips_data));
-});
-
+		promises.push(saveToFile('stop_times', stop_times_data));
+		promises.push(saveToFile('trips', trips_data));
+	})
+);
 {
 	let agencies = [];
 	agencies.push(['agency_id', 'agency_name', 'agency_url', 'agency_timezone']);
-	agencies.push([1, "Център за градска мобилност", 'https://sofiatraffic.bg', 'Europe/Sofia']);
+	agencies.push([1, GTFSConsts.agency.name, GTFSConsts.agency.site, GTFSConsts.timezone]);
 	promises.push(saveToFile('agency', agencies));
 }
 {
 	let feed_info = [];
 	feed_info.push(['feed_publisher_name', 'feed_publisher_url', 'feed_lang']);
-	feed_info.push(['Център за градска мобилност', 'https://sofiatraffic.bg', 'bg']);
+	feed_info.push([GTFSConsts.agency.name, GTFSConsts.agency.site, GTFSConsts.lang]);
 	promises.push(saveToFile('feed_info', feed_info));
 }
 {
