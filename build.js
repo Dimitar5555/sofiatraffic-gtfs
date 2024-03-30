11bf83ee15c9e90bd913d583bcab3cbb6c3ae2d2
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-03-30 19:24:51 +0200

    Update build.js

--- build.js
+++ build.js
@@ -16,7 +16,6 @@ const GTFSConsts = {
 	timezone: 'Europe/Sofia',
 	lang: 'bg'
 }
-var promises = [];
 
 function getJSON(file){
 	return fetch(repo+file)
@@ -35,96 +34,89 @@ function minsToTime(mins){
 }
 
 function saveToFile(name, data){
-	return fs.writeFile(`${outDir}/${name}.txt`, arrayToCSV(data), (err)=>{
-		if(err){
-			console.error(err)
-		}
-	});
+	let path = `${outDir}/${name}.txt`;
+	fs.writeFileSync(path, arrayToCSV(data));
+	console.log(`Done writing to ${path}`);
 }
 
 if (!fs.existsSync(outDir)){
     fs.mkdirSync(outDir);
 }
 
-promises.push(
-	getJSON('stops.json')
-	.then(data => {
-		let stops_data = [];
-		stops_data.push(['agency_id', 'stop_id', 'stop_name', 'stop_lat', 'stop_lon']);
-		data.forEach(stop => {
-			stops_data.push([1, stop.code, stop.names.bg.indexOf(',')!=-1?`"${stop.names.bg}"`:stop.names.bg, stop.coords[0], stop.coords[1]]);
-		});
-		saveToFile('stops', stops_data);
-	}
-));
+getJSON('stops.json')
+.then(data => {
+	let stops_data = [];
+	stops_data.push(['agency_id', 'stop_id', 'stop_name', 'stop_lat', 'stop_lon']);
+	data.forEach(stop => {
+		stops_data.push([1, stop.code, stop.names.bg.indexOf(',')!=-1?`"${stop.names.bg}"`:stop.names.bg, stop.coords[0], stop.coords[1]]);
+	});
+	saveToFile('stops', stops_data);
+});
 
-promises.push(
-	getJSON('routes.json')
-	.then(data => {
-		let routes_data = [];
-		routes_data.push(['agency_id', 'route_id', 'route_short_name', 'route_type']);
-		data.forEach((route, index) => {
-			routes_data.push([1, index+1, route.line, GTFSTypes[route.type]]);
-		});
-		saveToFile('routes', routes_data);
-	}
-));
+getJSON('routes.json')
+.then(data => {
+	let routes_data = [];
+	routes_data.push(['agency_id', 'route_id', 'route_short_name', 'route_type']);
+	data.forEach((route, index) => {
+		routes_data.push([1, index+1, route.line, GTFSTypes[route.type]]);
+	});
+	saveToFile('routes', routes_data);
+});
 
 
-var trips;
-var directions;
-promises.push(
-	getJSON('trips.json')
-	.then(data => {
-		trips = data;
-		return getJSON('directions.json');
-	})
-	.then(data => {
-		directions = data;
-		return getJSON('stop_times.json');
-	})
-	.then(data => {
-		let trips_data = [];
-		trips_data.push(['route_id', 'service_id', 'trip_id']);
-		
-		let stop_times_data = [];
-		stop_times_data.push(['trip_id', 'arrival_time', 'departure_time', 'stop_id', 'stop_sequence']);
-		
-		let last_trip = 0;
-		data.forEach(stop_times => {
-			last_trip++;
-			trips_data.push([trips[stop_times.trip].route_index+1, 1, last_trip]);
-			let stops = directions.find(dir => dir.code==trips[stop_times.trip].direction).stops;
-			stop_times.times.forEach((stop_time, index) => {
-				stop_times_data.push([last_trip, minsToTime(stop_time), minsToTime(stop_time), stops[index], index+1]);
-			});
+
+let local_promises = [];
+local_promises.push(getJSON('trips.json'));
+local_promises.push(getJSON('directions.json'));
+local_promises.push(getJSON('stop_times.json'));
+
+
+Promise.all(local_promises)
+.then((datas) => {
+	var trips = datas[0]
+	var directions = datas[1];
+	var data = datas[2];
+	
+	let trips_data = [];
+	trips_data.push(['route_id', 'service_id', 'trip_id']);
+	
+	let stop_times_data = [];
+	stop_times_data.push(['trip_id', 'arrival_time', 'departure_time', 'stop_id', 'stop_sequence']);
+	
+	let last_trip = 0;
+	data.forEach(stop_times => {
+		last_trip++;
+		trips_data.push([trips[stop_times.trip].route_index+1, 1, last_trip]);
+		let stops = directions.find(dir => dir.code==trips[stop_times.trip].direction).stops;
+		stop_times.times.forEach((stop_time, index) => {
+			stop_times_data.push([last_trip, minsToTime(stop_time), minsToTime(stop_time), stops[index], index+1]);
 		});
-		promises.push(saveToFile('stop_times', stop_times_data));
-		promises.push(saveToFile('trips', trips_data));
-	})
-);
+	});
+
+	saveToFile('trips', trips_data);
+	saveToFile('stop_times', stop_times_data);
+})
+.finally(() => {
+	console.log('Almost done');	
+	zipper.sync.zip("./result/").compress().save("result.zip");
+	console.log('Zipped data to result.zip');
+})
 {
 	let agencies = [];
 	agencies.push(['agency_id', 'agency_name', 'agency_url', 'agency_timezone']);
 	agencies.push([1, GTFSConsts.agency.name, GTFSConsts.agency.site, GTFSConsts.timezone]);
-	promises.push(saveToFile('agency', agencies));
+	saveToFile('agency', agencies);
 }
 {
 	let feed_info = [];
 	feed_info.push(['feed_publisher_name', 'feed_publisher_url', 'feed_lang']);
 	feed_info.push([GTFSConsts.agency.name, GTFSConsts.agency.site, GTFSConsts.lang]);
-	promises.push(saveToFile('feed_info', feed_info));
+	saveToFile('feed_info', feed_info);
 }
 {
 	let calendar = [];
 	calendar.push(['service_id', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'start_date', 'end_date']);
 	let year = new Date().getFullYear();
 	calendar.push([1, 1, 1, 1, 1, 1, 1, 1, `${year}-01-01`, `${year}-12-31`]);
-	promises.push(saveToFile('calendar', calendar));
+	saveToFile('calendar', calendar);
 }
-
-promises.push(new Promise(resolve => setTimeout(resolve, 20*1000)))
-Promise.all(promises)
-.then(() => {
-	zipper.sync.zip("./result/").compress().save("result.zip");
-});
