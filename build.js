21d4548ed820088f4e50f7b48c14c49dfd3e90bc
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2024-03-30 15:05:15 +0200

    Add files via upload

--- build.js
+++ build.js
@@ -1,4 +1,5 @@
-const fs = require('fs')
+const fs = require('fs');
+const zipper = require('zip-local');
 const repo = 'https://raw.githubusercontent.com/Dimitar5555/sofiatraffic-schedules/master/docs/data/';
 const outDir = './result';
 const GTFSTypes = {
@@ -24,6 +25,10 @@ function minsToTime(mins){
 	return `${hour}:${min}:${secs}`
 }
 
+function saveToFile(name, data){
+	fs.writeFileSync(`${outDir}/${name}.txt`, arrayToCSV(data));
+}
+
 if (!fs.existsSync(outDir)){
     fs.mkdirSync(outDir);
 }
@@ -31,32 +36,26 @@ if (!fs.existsSync(outDir)){
 getJSON('stops.json')
 .then(data => {
 	let stops_data = [];
-	stops_data.push(['stop_id', 'stop_name', 'stop_lat', 'stop_lon']);
+	stops_data.push(['agency_id', 'stop_id', 'stop_name', 'stop_lat', 'stop_lon']);
 	data.forEach(stop => {
-		stops_data.push([stop.code, stop.names.bg.indexOf(',')!=-1?`"${stop.names.bg}"`:stop.names.bg, stop.coords[0], stop.coords[1]]);
+		stops_data.push([1, stop.code, stop.names.bg.indexOf(',')!=-1?`"${stop.names.bg}"`:stop.names.bg, stop.coords[0], stop.coords[1]]);
 	});
-	fs.writeFileSync(`${outDir}/stops.csv`, arrayToCSV(stops_data));
+	saveToFile('stops', stops_data);
 });
 
 getJSON('routes.json')
 .then(data => {
 	let routes_data = [];
-	routes_data.push(['route_id', 'route_short_name', 'route_type']);
+	routes_data.push(['agency_id', 'route_id', 'route_short_name', 'route_type']);
 	data.forEach((route, index) => {
-		routes_data.push([index+1, route.line, GTFSTypes[route.type]]);
+		routes_data.push([1, index+1, route.line, GTFSTypes[route.type]]);
 	});
-	fs.writeFileSync(`${outDir}/routes.csv`, arrayToCSV(routes_data));
+	saveToFile('routes', routes_data);
 });
 var trips;
 getJSON('trips.json')
 .then(data => {
-	let trips_data = [];
 	trips = data;
-	trips_data.push(['route_id', 'trip_id']);
-	data.forEach((trip, index) => {
-		trips_data.push([trip.route_index+1, index+1]);
-	});
-	fs.writeFileSync(`${outDir}/trips.csv`, arrayToCSV(trips_data));
 });
 
 var directions;
@@ -66,13 +65,52 @@ getJSON('directions.json')
 });
 getJSON('stop_times.json')
 .then(data => {
+	let last_trip = 1;
 	let stop_times_data = [];
+	let trips_data = [];
+	trips_data.push(['route_id', 'service_id', 'trip_id']);
 	stop_times_data.push(['trip_id', 'arrival_time', 'departure_time', 'stop_id', 'stop_sequence']);
 	data.forEach(stop_times => {
+		last_trip++;
+		trips_data.push([trips[stop_times.trip].route_index+1, 1, last_trip]);
+		let stops = directions.find(dir => dir.code==trips[stop_times.trip].direction).stops;
 		stop_times.times.forEach((stop_time, index) => {
-			stop_times_data.push([stop_times.trip+1, minsToTime(stop_time), minsToTime(stop_time), directions.find(dir => dir.code==trips[stop_times.trip].direction).stops[index], index+1]);
-
+			stop_times_data.push([last_trip, minsToTime(stop_time), minsToTime(stop_time), stops[index], index+1]);
 		});
 	});
-	fs.writeFileSync(`${outDir}/stop_times.csv`, arrayToCSV(stop_times_data));
+	saveToFile('stop_times', stop_times_data);
+	saveToFile('trips', trips_data);
 });
+
+{
+	function getLastSunday(year, month) {
+		let d = new Date(year, month, 0);
+		d.setDate(d.getDate() - d.getDay());
+		return d;
+	}
+
+	let today = new Date();
+	let dst_start = getLastSunday(today.getFullYear(), 3);
+	let dst_end = getLastSunday(today.getFullYear(), 11);
+	let is_dst = dst_start <= today && today <= dst_end;
+	let offset = is_dst?3:2;
+	let agencies = [];
+	agencies.push(['agency_id', 'agency_name', 'agency_url', 'agency_timezone']);
+	agencies.push([1, "Център за градска мобилност", "https://sofiatraffic.bg", offset]);
+	saveToFile('agency', agencies);
+}
+{
+	let feed_info = [];
+	feed_info.push(['feed_publisher_name', 'feed_publisher_url', 'feed_lang']);
+	feed_info.push(['Център за градска мобилност', 'https://sofiatraffic.bg', 'bul']);
+	saveToFile('feed_info', feed_info);
+}
+{
+	let calendar = [];
+	calendar.push(['service_id', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'start_date', 'end_date']);
+	let year = new Date().getFullYear();
+	calendar.push([1, 1, 1, 1, 1, 1, 1, 1, `${year}-01-01`, `${year}-12-31`]);
+	saveToFile('calendar', calendar);
+}
+
+zipper.sync.zip("./result/").compress().save("result.zip");
